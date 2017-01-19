82f832806c4c70a26352267b3dbb085af6157fc5
Ishkov Victor <VYuIshkov.SBT@sberbank.ru>
2017-01-19 12:44:34 +0300

    add Main.java and TestMain.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author SBT-Ishkov-VYu
+ * @since 19.01.2017
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/TestMain.java
@@ -0,0 +1,6 @@
+/**
+ * @author SBT-Ishkov-VYu
+ * @since 20.01.2017
+ */
+public class TestMain {
+}
